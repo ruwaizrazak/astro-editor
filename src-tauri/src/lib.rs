c8149d8bc81ccdf1354471e6cdaf8f6b500ae24a
Danny Smith <hi@danny.is>
2025-07-24 20:13:54 +0100

    Set up Tauri Build Signing and updator

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -53,6 +53,7 @@ async fn update_format_menu_state(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
